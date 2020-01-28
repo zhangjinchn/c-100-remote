96187271759f6e7a9474347c2c6b4159cb597f02
zhangjin <zhangjin.chn@qq.com>
2020-01-28 19:38:26 +0800

    c语言经典100例子

---
+++ c语言第1例.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int i,j,k;
+	{
+	for(i=1;i<=4;i++)
+	
+	    for(j=1;j<=4;j++)
+	    
+	    for(k=1;k<=4;k++)
+	    
+    
+    if(i!=j&j!=k&k!=i)
+    printf("%d%d%d   ",i,j,k);	
+   }
+}
