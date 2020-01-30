0951347841de764ee34b203653048fe7f36816c0
zhangjin <zhangjin.chn@qq.com>
2020-01-30 20:45:51 +0800

    cyuyan

---
+++ c语言第4例.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int n,i,j,k;
+	printf("输出金字塔的行数："); 
+	scanf("%d",&n);
+	for(i=n;i>=1;i--)
+	{
+		for(j=1;j<=n-i;j++)
+		printf(" ");
+		for(k=1;k<=2*i-1;k++)
+		printf("*");
+		printf("\n");
+	
+	} 
+ } 
